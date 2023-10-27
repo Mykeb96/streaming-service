6d6de55f3c54eb3b681a5c63d81a5dc4eb360fb2
unknown <mykael.barnes@hotmail.com>
2023-10-27 12:09:09 -0700

    maybe final commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "streamingservice": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3streamingservicestorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,38 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { Storage } from "@aws-amplify/storage"
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import awsExports from '../src/aws-exports';
+import {useState, useEffect} from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+
+
 
 export default function Home() {
+
+Amplify.configure(awsExports);
+
+const [data, setData] = useState<any>([])
+const [selectedVideo, setSelectedVideo] = useState<any>('')
+
+
+  useEffect(() => {
+
+    Storage.list('') // for listing ALL files without prefix, pass '' instead
+      .then(({ results }) => {
+        console.log(results)
+        setData(results)
+        setSelectedVideo(results[1].key)
+      })
+      .catch((err) => console.log(err));
+   
+  }, [])
+  
+  
+
+
   return (
     <>
       <Head>
@@ -14,100 +41,52 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main className={styles.main}>
+
+        <div className={styles.content_container}>
+
+          <div className={styles.video_player_container}>
+
+            {data.length > 0 ?
+              <div>
+                <video height={500} width={900} controls key={selectedVideo} className={styles.video}>
+                  <source src={`https://d2by0efpla5ofl.cloudfront.net/public/${selectedVideo}`}/>
+                </video>
+              </div> 
+            : 
+              <div>loading videos...</div>
+            }
+
           </div>
+
+          <div className={styles.video_list_container}>
+            {data.map((el: any, index: number) => el.key != '' ? <span className={styles.video_list_element} onClick={() => setSelectedVideo(el.key)}>{el.key.replace('.mp4', '')}</span> : null)}
+
+          </div>
+
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+      {/* <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+      </Authenticator> */}
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+        {/* {data.length > 0 ?
+          <div>
+            <video height={400} width={700} controls>
+              <source src={`https://d2by0efpla5ofl.cloudfront.net/public/${data[1]?.key}`}/>
+            </video>
+          </div> 
+        : 
+          <div>loading videos...</div>
+        } */}
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        
       </main>
     </>
   )
